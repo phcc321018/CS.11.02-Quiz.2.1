fd821939ff5606035293c4154d7ff5bc577e35b7
phcc321018 <phcc32@student.cis.edu.hk>
2023-09-21 09:02:42 +0800

    quiz

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
 public class Main {
-
     public static void main(String[] args) {
-
+        int numOne = 5;
+        int numTwo = 10;
+        int numThree = 15;
+        int numFour = 20;
+        int resultThree = add(numOne, numTwo, numThree, numFour);
+        System.out.println(resultThree);
     }
 
+    public static int add(int numOne, int numTwo, int numThree, int numFour) {
+        int sumOne = add(numOne, numTwo);
+        int sumTwo = add(sumOne, numThree);
+        int sumThree = add(sumTwo, numFour);
+        return sumThree;
+    }
+
+    public static int add(int numOne, int numTwo) {
+        return numOne + numTwo;
+    }
 }
